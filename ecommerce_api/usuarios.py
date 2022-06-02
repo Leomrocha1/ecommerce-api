f0373a6e37d5122267cf3c3d30c087b80daa847d
Leonardo Rocha <leonardo.rocha@sinqia.com.br>
2022-06-02 16:25:51 -0300

    feat(usuarios): criado crud de usuários

--- ecommerce_api/usuarios.py
+++ ecommerce_api/usuarios.py
@@ -8,9 +8,11 @@ app = Blueprint('usuario', __name__, url_prefix='/v1/usuario')
 
 @app.route('', methods=['POST'])
 def criar_usuario():
+    """
+    Cria um novo usuário.
+    """
     try:
-        dados = UsuarioSchema.load(request.json)
-        print(dados)
+        dados = UsuarioSchema().load(request.json)
     except ValidationError as err:
         return jsonify({'erros': err.messages}), 400
 
@@ -22,10 +24,14 @@ def criar_usuario():
     except ValidationError as err:
         return jsonify({'erros': err.messages}), 500
 
+
     return jsonify(UsuarioSchema().dump(usuario)), 201
 
 @app.route('', methods=['GET'])
 def get_usuarios():
+    """
+    Retorna todos os usuários.
+    """
     usuarios = Usuarios.query.all()
 
     if not usuarios:
@@ -34,3 +40,53 @@ def get_usuarios():
     resultado = jsonify(UsuarioSchema().dump(usuarios, many=True))
 
     return resultado
+
+@app.route('/<int:id>', methods=['GET'])
+def get_usuario(id):
+    """
+    Retorna um usuário pelo ID.
+    """
+    usuario = Usuarios.query.get(id)
+
+    if not usuario:
+        return jsonify({'mensagem': 'Usuário não encontrado.'}), 404
+
+    return jsonify(UsuarioSchema().dump(usuario))
+
+
+@app.route('/<int:id>', methods=['PATCH'])
+def atualizar_usuario(id):
+    """
+    Atualiza um usuário pelo ID.
+    """
+    usuario = Usuarios.query.get(id)
+
+    if not usuario:
+        return jsonify({'mensagem': 'Usuário não encontrado'}), 404
+
+    try:
+        dados = UsuarioSchema(partial=True).load(request.json)
+    except ValidationError as err:
+        return jsonify({'erros': err.messages}), 400
+
+    for chave, valor in dados.items():
+        setattr(usuario, chave, valor)
+
+    db.session.commit()
+
+    return jsonify(UsuarioSchema().dump(usuario))
+
+@app.route('/<int:id>', methods=['DELETE'])
+def deletar_usuario(id):
+    usuario = Usuarios.query.get(id)
+
+    if not usuario:
+        return jsonify({'mensagem': 'Usuário não encontrado'}), 404
+
+    try:
+        db.session.delete(usuario)
+        db.session.commit()
+    except ValidationError as err:
+        return jsonify({'erros': err.messages}), 500
+
+    return jsonify({'mensagem': 'Usuário deletado com sucesso'}), 204
